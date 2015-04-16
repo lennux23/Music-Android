528e0404d41fd65a878dfa5817a63b37aefa8cc8
MobileStudio06 <mobilestudio06@iMacdeMStudio06.lan>
2015-04-15 20:24:20 -0500

    programar boton play

--- app/src/main/java/spartdark/com/mx/playmusic/Track.java
+++ app/src/main/java/spartdark/com/mx/playmusic/Track.java
@@ -17,14 +17,17 @@ public class Track {
     private String artist;
     private String coverName;
     private String time;
+    private String urlTrack;
 
 
-    public Track(Drawable cover, String name, String artist, String coverName, String time) {
+
+    public Track(Drawable cover, String name, String artist, String coverName, String time, String urlTrack) {
         this.cover = cover;
         this.name = name;
         this.artist = artist;
         this.coverName = coverName;
         this.time = time;
+        this.urlTrack = urlTrack;
     }
 
 
@@ -33,13 +36,13 @@ public class Track {
 
         ArrayList<Track> arrayContact= new ArrayList<Track>();
 
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.proanio),"Argentum","Enjambre","Proaño","3:15"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.proanio),"Sábado Perpetuo","Enjambre","Proaño","5:14"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.monitor_volovan),"Monitor","Volovan","Monitor","3:10"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.safe_and_sound_capitalcities),"Safe & Sound","Capital Cities","In a tidal wave of mystery","4:03"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.safe_and_sound_capitalcities),"I sold my bed","Capital Cities","In a tidal wave of mystery","3:67"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.siddhartha_siddhartha),"Control","Siddhartha","Siddhartha","2:56"));
-        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.soy_como_quiero_ser_luis_miguel),"Ahora te puedes marchar","Luis Miguel","Soy como quiero ser","3:10"));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.proanio),"Argentum","Enjambre","Proaño","3:15", "enjambre_argentum"));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.proanio),"Sábado Perpetuo","Enjambre","Proaño","5:14","enjambre_sabado_perpetuo"));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.monitor_volovan),"Monitor","Volovan","Monitor","3:10","volovan_monitor" ));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.safe_and_sound_capitalcities),"Safe & Sound","Capital Cities","In a tidal wave of mystery","4:03","capital_cities_safe_and_cound"));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.safe_and_sound_capitalcities),"I sold my bed","Capital Cities","In a tidal wave of mystery","3:67","capital_cities_i_sold_my_bed_but_not_my_stereo" ));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.siddhartha_siddhartha),"Control","Siddhartha","Siddhartha","2:56","siddhartha_control"));
+        arrayContact.add(new Track(context.getResources().getDrawable(R.mipmap.soy_como_quiero_ser_luis_miguel),"Ahora te puedes marchar","Luis Miguel","Soy como quiero ser","3:10", "luis_miguel_ahora_te_puedes_marchar"));
 
 
 
@@ -86,5 +89,13 @@ public class Track {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public String getUrlTrack() {
+        return urlTrack;
+    }
+
+    public void setUrlTrack(String urlTrack) {
+        this.urlTrack = urlTrack;
     }
 }
--- app/src/main/java/spartdark/com/mx/playmusic/TrackDetailActivity.java
+++ app/src/main/java/spartdark/com/mx/playmusic/TrackDetailActivity.java
@@ -1,17 +1,27 @@
 package spartdark.com.mx.playmusic;
 
 import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class TrackDetailActivity extends ActionBarActivity {
+public class TrackDetailActivity extends ActionBarActivity implements View.OnClickListener {
     private int trackPosition;
     private Track track;
+    private ImageButton actionPlay, actionBackward, actionForward;
+    private boolean isPlaying = false;
+    private String song;
+
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +29,9 @@ public class TrackDetailActivity extends ActionBarActivity {
 
         Intent intent = getIntent();
         trackPosition = intent.getIntExtra("TRACK", 0);
-        track = Track.getTracks(getApplicationContext()).get(trackPosition);
 
+        track = Track.getTracks(getApplicationContext()).get(trackPosition);
+        song = track.getUrlTrack();
         ImageView imgDetailCover = (ImageView) findViewById(R.id.imgDetailCover);
 
         TextView tvDetailName = (TextView) findViewById(R.id.tvDetailName);
@@ -34,10 +45,48 @@ public class TrackDetailActivity extends ActionBarActivity {
         tvDetailCover.setText(track.getCoverName());
         tvDetaiTime.setText(track.getTime());
 
+        actionPlay = (ImageButton) findViewById(R.id.actionPlay);
+        actionBackward = (ImageButton) findViewById(R.id.actionBackward);
+        actionForward = (ImageButton) findViewById(R.id.actionForward);
 
+        actionPlay.setOnClickListener(this);
+        actionBackward.setOnClickListener(this);
+        actionForward.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        MediaMetadataRetriever metadatos = new MediaMetadataRetriever();
+        switch (v.getId()){
+            case R.id.actionPlay:
+
+                if (isPlaying){
+                    Log.v("log", "termina de reproducir");
+                    mediaPlayer.stop();
+                    actionPlay.setImageResource(R.mipmap.play);
+                    isPlaying = false;
+
+                } else {
+                    //mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.capital_cities_i_sold_my_bed_but_not_my_stereo);
+                    mediaPlayer = MediaPlayer.create(TrackDetailActivity.this,  getResources().getIdentifier("raw/"+song,
+                            "raw", getPackageName()));
+                    Log.v("log", "comienza a reproducir");
+                    Log.v("song", "song "+ song);
+                    mediaPlayer.start();
+                    actionPlay.setImageResource(R.mipmap.stop_play);
+                    isPlaying = true;
+
+                }
+                break;
+            case R.id.actionBackward:
+                break;
+            case R.id.actionForward:
+                break;
+
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
